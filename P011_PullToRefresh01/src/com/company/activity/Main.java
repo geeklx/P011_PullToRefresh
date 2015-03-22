aada933e651c3ac42fa0ec8b8fc69253a5824ec5
geeklx <liangxiao6@live.com>
2015-03-22 21:52:40 +0800

    P011_PullToRefreshDemo
    下拉刷新

---
+++ P011_PullToRefresh01/src/com/company/activity/Main.java
@@ -0,0 +1,184 @@
+package com.company.activity;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import android.app.ListActivity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnLastItemVisibleListener;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.State;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.handmark.pulltorefresh.library.extras.SoundPullEventListener;
+
+public class Main extends  ListActivity {
+
+	static final int MENU_MANUAL_REFRESH = 0;
+	static final int MENU_DISABLE_SCROLL = 1;
+	static final int MENU_SET_MODE = 2;
+	static final int MENU_DEMO = 3;
+	//当前默认的滑动模式
+	private Mode mDefaultMode;
+	
+	private LinkedList<String> mListItems;
+	private PullToRefreshListView mPullRefreshListView;
+	private ArrayAdapter<String> mAdapter;
+	
+	//列表项
+	private String[] mStrings = { "aaaa", "111111", "222222", "333333", "444444",
+			"55555", "66666", "7777 777"};
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
+	
+		//获取当前的模式
+		mDefaultMode=mPullRefreshListView.getMode();
+		
+		// 设置一个侦听器被调用时应刷新列表
+		mPullRefreshListView.setOnRefreshListener(new OnRefreshListener<ListView>() {
+			@Override
+			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+				String label = DateUtils.formatDateTime(getApplicationContext(), System.currentTimeMillis(),
+						DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+
+				// 更新最后的标签
+				refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
+			  
+				new GetDataTask().execute();
+			}
+		});
+
+		//添加一个列表最后的监听器
+		mPullRefreshListView.setOnLastItemVisibleListener(new OnLastItemVisibleListener() {
+
+			@Override
+			public void onLastItemVisible() {
+				Toast.makeText(Main.this, "已经是最后的了", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		ListView actualListView = mPullRefreshListView.getRefreshableView();
+
+		// Need to use the Actual ListView when registering for Context Menu
+		registerForContextMenu(actualListView);
+
+		mListItems = new LinkedList<String>();
+		mListItems.addAll(Arrays.asList(mStrings));
+
+		mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mListItems);
+
+		/**
+		 * 添加声音事件监听器
+		 */
+		SoundPullEventListener<ListView> soundListener = new SoundPullEventListener<ListView>(this);
+		mPullRefreshListView.setOnPullEventListener(soundListener);
+
+		// You can also just use setListAdapter(mAdapter) or
+		// mPullRefreshListView.setAdapter(mAdapter)
+		actualListView.setAdapter(mAdapter);
+	}
+
+	private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+
+		@Override
+		protected String[] doInBackground(Void... params) {
+			// Simulates a background job.
+			try {
+				Thread.sleep(4000);
+			} catch (InterruptedException e) {
+			}
+			return mStrings;
+		}
+
+		@Override
+		protected void onPostExecute(String[] result) {
+			mListItems.addFirst("yjyjyjyjyjyj");
+			mAdapter.notifyDataSetChanged();
+
+			// 列表刷新完成
+			mPullRefreshListView.onRefreshComplete();
+			super.onPostExecute(result);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, MENU_MANUAL_REFRESH, 0, "Manual Refresh");
+		menu.add(0, MENU_DISABLE_SCROLL, 1,
+				mPullRefreshListView.isScrollingWhileRefreshingEnabled() ? "Disable Scrolling while Refreshing"
+						: "Enable Scrolling while Refreshing");
+		menu.add(0, MENU_SET_MODE, 0, mPullRefreshListView.getMode() == Mode.BOTH ? "Change to MODE_PULL_DOWN"
+				: "Change to MODE_PULL_BOTH");
+		menu.add(0, MENU_DEMO, 0, "Demo");
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
+
+		menu.setHeaderTitle("Item: " + getListView().getItemAtPosition(info.position));
+		menu.add("Item 1");
+		menu.add("Item 2");
+		menu.add("Item 3");
+		menu.add("Item 4");
+
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		MenuItem disableItem = menu.findItem(MENU_DISABLE_SCROLL);
+		disableItem
+				.setTitle(mPullRefreshListView.isScrollingWhileRefreshingEnabled() ? "Disable Scrolling while Refreshing"
+						: "Enable Scrolling while Refreshing");
+
+		MenuItem setModeItem = menu.findItem(MENU_SET_MODE);
+		setModeItem.setTitle(mPullRefreshListView.getMode() == Mode.BOTH ? "Change to MODE_FROM_START"
+				: "Change to MODE_PULL_BOTH");
+
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+			case MENU_MANUAL_REFRESH:
+				new GetDataTask().execute();
+				mPullRefreshListView.setRefreshing(false);
+				break;
+			case MENU_DISABLE_SCROLL:
+				mPullRefreshListView.setScrollingWhileRefreshingEnabled(!mPullRefreshListView
+						.isScrollingWhileRefreshingEnabled());
+				break;
+			case MENU_SET_MODE:
+				mPullRefreshListView.setMode(mPullRefreshListView.getMode() == Mode.BOTH ? Mode.PULL_FROM_START
+						: Mode.BOTH);
+				break;
+			case MENU_DEMO:
+				mPullRefreshListView.demo();
+				break;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+}
